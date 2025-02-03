749f180ae7b88bcdeaf1f2451024033542a87dff
MoAwad <mawad8603@.com>
2025-02-03 22:33:07 +0200

    Add firebase configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:chat_app/features/splash/presentation/views/app_loader.dart';
+import 'package:chat_app/firebase_options.dart';
 import 'package:device_preview/device_preview.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(DevicePreview(builder: (context) => const MyApp()));
 }
 
