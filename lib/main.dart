c0fa22002f1a2e8e42ea797ae3d85d83bc6fd7f6
MoAwad <mawad8603@.com>
2025-02-03 03:00:16 +0200

    Initialize app structure & Finish splash screen

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:chat_app/features/splash/presentation/views/app_loader.dart';
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(DevicePreview(builder: (context) => const MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
+      home:const AppLoader(),
+    );
+  }
+}
